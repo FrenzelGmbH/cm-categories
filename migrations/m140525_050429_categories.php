d8f105b4921b6d60491b07dc9a8abcfa41ae7725
Philipp Frenzel <philipp@frenzel.net>
2014-05-27 21:19:38 +0200

    clean migrations to work correct

--- migrations/m140525_050429_categories.php
+++ migrations/m140525_050429_categories.php
@@ -9,7 +9,7 @@
 
 use yii\db\Schema;
 
-class m140515_050429_communicationtables extends \yii\db\Migration
+class m140525_050429_categories extends \yii\db\Migration
 {
 	public function up()
 	{
@@ -25,12 +25,9 @@ class m140515_050429_communicationtables extends \yii\db\Migration
         throw new RuntimeException('Your database is not supported!');
     }
 
-		$this->createTable('{{%communication}}',[
+		$this->createTable('{{%categories}}',[
       'id'                => Schema::TYPE_PK,
-      'mobile'            => Schema::TYPE_STRING .'(200)',
-      'phone'             => Schema::TYPE_STRING .'(200)',
-      'fax'               => Schema::TYPE_STRING .'(200)',
-      'email'             => Schema::TYPE_STRING .'(200)',
+      'name'              => Schema::TYPE_STRING .'(200)',
       //possible reference to user
       'user_id'           => Schema::TYPE_INTEGER.' NULL',
       //module fields
@@ -45,34 +42,15 @@ class m140515_050429_communicationtables extends \yii\db\Migration
       'updated_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
       'deleted_at'        => Schema::TYPE_INTEGER . ' DEFAULT NULL',
       //Foreign Keys
-      'communication_type_id' => Schema::TYPE_INTEGER,      
+      'parent'            => Schema::TYPE_INTEGER.' NULL',      
     ],$tableOptions);
-
-    $this->createTable('{{%communication_type}}',[
-      'id'                => Schema::TYPE_PK,
-      'name'              => Schema::TYPE_STRING .'(100)',
-      //possible reference to user
-      'user_id'           => Schema::TYPE_INTEGER.' NULL',
-      //interface fields
-      'system_key'        => Schema::TYPE_STRING .'(100)',
-      'system_name'       => Schema::TYPE_STRING .'(100)',
-      'system_upate'      => Schema::TYPE_INTEGER.' DEFAULT NULL',
-      // timestamps
-      'created_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
-      'updated_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
-      'deleted_at'        => Schema::TYPE_INTEGER . ' DEFAULT NULL'
-    ],$tableOptions);
-
-    $this->addForeignKey('fk_communication_communication_type', '{{%communication}}', 'communication_type_id', '{{%communication_type}}', 'id', 'CASCADE', 'RESTRICT');
 
 	}
 
 	public function down()
 	{
 		//drop FK's first
-    $this->dropForeignKey('fk_communication_communication_type', '{{%communication}}');
-
-    $this->dropTable('{{%communication_type}}');
-		$this->dropTable('{{%communication}}');
+    
+    $this->dropTable('{{%categories}}');
 	}
 }
