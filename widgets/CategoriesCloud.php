191a02ebdeb6b3237bdbf1d56f6c552fa2ad08a0
Philipp Frenzel <philipp@frenzel.net>
2014-05-31 15:12:10 +0200

    cleaning up and adding documentation

--- widgets/CategoriesCloud.php
+++ widgets/CategoriesCloud.php
@@ -8,14 +8,36 @@ use frenzelgmbh\cmcategories\models\Categories;
 
 class CategoriesCloud extends Portlet
 {
-  public $title  = 'Categroies';
+  /**
+   * const WIDGET_NAME must be defined for all widgets!
+   */
+  const WIDGET_NAME = 'CATEGORIES_CLOUD';
+
+  /**
+   * the title, that will be diplayed in the portlet head
+   * @param string $title
+   */
+  public $title  = 'Categories';
+  
+  /**
+   * module for which the category will be looked up
+   */
   public $module = NULL;
+
+  /**
+   * the styling for the links
+   */
   public $linkclass  = NULL;
   
-  public function init() {
-    parent::init();
-  }
+  /**
+   * target that will be used to jump to from the cloud
+   * @param string $target
+   */
+  public $target = '/site/index';  
 
+  /**
+   * renders the content for the widget
+   */
   protected function renderContent()
   {
     if(!is_null($this->module))
@@ -23,7 +45,7 @@ class CategoriesCloud extends Portlet
       $categories = Categories::getOptions($this->module);
       foreach($categories as $key=>$value)
       {
-        echo Html::a(strtoupper(Html::encode($value)), array('/site/index','category'=>$key,'reference'=>Html::encode($value)), array('class'=>$this->linkclass))
+        echo Html::a(strtoupper(Html::encode($value)), array($this->target,'category'=>$key,'reference'=>Html::encode($value)), array('class'=>$this->linkclass))
         ." // \n";
       }
     }
